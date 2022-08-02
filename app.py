338d3c5fb9d223490187f1b875df3dd16e03bbce
tony <tonychi123@gmail.com>
2022-08-02 14:31:17 +0800

    deploy: freeze requirments

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(str(os.environ.get("ACCESS_TOKEN")))
 handler = WebhookHandler(str(os.environ.get("SECRET")))
 
+@app.route("/")
+def index():
+    return "Hello, world!"
 
 @app.route("/callback", methods=['POST','GET'])
 def callback():
@@ -46,4 +49,4 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0')
