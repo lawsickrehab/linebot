8038ee4c6a81651ebdd82cd34f9058d46eaae514
tony <tonychi123@gmail.com>
2022-08-06 17:08:13 +0800

    feat: merge

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
 import requests
 
-def requestFile(ACCESS_TOKEN, mid):
+from __main__ import ACCESS_TOKEN
+
+def requestFile(mid,ack=ACCESS_TOKEN):
     url=f'https://api-data.line.me/v2/bot/message/{mid}/content'
     headers={
-        "Authorization": "Bearer "+ACCESS_TOKEN
+        "Authorization": "Bearer "+ack
     }
     return requests.get(url,headers=headers)
 
