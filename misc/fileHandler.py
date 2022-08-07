f7ecbead22bc3ab5ad3eebd286fb46db73df5b4b
ycwang <ycwang.cs@gmail.com>
2022-08-07 22:53:56 +0800

    fix tar bug

--- misc/fileHandler.py
+++ misc/fileHandler.py
@@ -1,18 +1,34 @@
+from genericpath import isfile
 import os
+import tarfile
 
 class FileHandler:
     def checkAsset():
         if os.path.isdir(f"asset") == False:
             os.mkdir("asset")
     def unzip(UID):
-        if os.path.isfile(f"asset/{UID}.tar"):
-            os.system(f"tar xfv asset/{UID}.tar")
-        else:
-            os.mkdir(f"asset/{UID}")
-    def zip(UID):
+        os.chdir(f"asset")
+        os.mkdir(f"{UID}")
+        if os.path.isfile(f"{UID}.tar"):
+            os.system(f"mv {UID}.tar {UID}/{UID}.tar")
+            os.chdir(f"{UID}")
+            os.system(f"tar xvf {UID}.tar > tar.log")
+            if os.path.isfile("tar.log"):
+                os.system(f"rm tar.log")
+            os.system(f"rm {UID}.tar")
+            os.chdir("..")
+        os.chdir("..")
+
+    def zip(UID, MID):
         if os.path.isfile(f"asset/{UID}.tar"):
             os.system(f"rm asset/{UID}.tar")
-        os.system(f"tar cfv asset/{UID}.tar asset/{UID}")
+        os.chdir(f"asset/{UID}")
+        os.system(f"tar cvf {UID}.tar * > tar.log")
+        if os.path.isfile("tar.log"):
+            os.system(f"rm tar.log")
+        os.chdir("..")
+        os.system(f"mv {UID}/{UID}.tar {UID}.tar")
+        os.chdir("..")
         os.system(f"rm -r asset/{UID}")
     
     def addFile(UID, MID, content):
@@ -22,8 +38,8 @@ class FileHandler:
     def driver(UID, MID, content):
         FileHandler.checkAsset()
         FileHandler.unzip(UID)
-        # content = FileHandler.load(UID, "test.JPEG")
+        content = "test"
         FileHandler.addFile(UID, MID, content)
-        FileHandler.zip(UID)
+        FileHandler.zip(UID, MID)
 
-# FileHandler.driver(1, 2, content)
+FileHandler.driver(2, 5, "")
