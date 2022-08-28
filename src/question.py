8e1d2b87fe92c888dd33ff08ecc5adbf9292e078
tony <tonychi123@gmail.com>
2022-08-28 16:13:39 +0800

    style: remove unused function

--- src/question.py
+++ src/question.py
@@ -2,16 +2,10 @@ import json
 from linebot.models import *
 
 class Question:
-    def __init__(self):
-        pass
 
     def read_json(fname):
         with open(fname,'r') as jfile:
             contents=json.load(jfile)
-        return contents
-    def read_txt(fname):
-        with open(fname,'r') as tfile:
-            contents=tfile.read()
         return contents
 
     def get(qid):
