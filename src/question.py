d46f4e5f77ec8b148776cf4731ef2df99ecb1602
tony <tonychi123@gmail.com>
2022-08-28 16:57:44 +0800

    refactor: change question class

--- src/question.py
+++ src/question.py
@@ -2,15 +2,21 @@ import json
 from linebot.models import *
 
 class Question:
+    def __init__(self,qid):
+        raw=self.get(qid)
+        self.id=raw['id']
+        self.question=raw['question']
+        self.options=raw['options']
+        self.key=raw['key']
+        
 
-    def read_json(fname):
+    def read_json(self,fname):
         with open(fname,'r') as jfile:
             contents=json.load(jfile)
         return contents
 
-    def get(qid):
+    def get(self,qid):
         qid=str(qid)
         fname=qid+'.json'
-        return Question.read_json('resources/questions/'+fname)
-
+        return self.read_json('resources/questions/'+fname)
 
